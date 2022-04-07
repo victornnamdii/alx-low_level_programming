986daa1c505e040f652cb1d84e8d9538e1f4b0df
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-07 13:49:10 -0700

    Edit:4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int main ()
+/* program that prints exactly Programming is like building a multilingual puzzle with quotation mark */
+
+int main(void)
 {
 	/* initializing strings */
 
-	char str1[]=  "Programming is like building a multilingual puzzle";
+	char str1[] =  "\"Programming is like building a multilingual puzzle";
 
 	puts(str1);
 
