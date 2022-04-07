735cab3f42a1f104a7c352b00edf44ea8bd83e74
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-07 15:09:42 -0700

    Add:101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	char qte[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	putchar(qte);
+
+	return (0);
+}
