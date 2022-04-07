2c67fbad7d5d840d06371497efc384841c09bb25
victornnamdii <101352050+victornnamdii@users.noreply.github.com>
2022-04-07 23:46:47 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 /**
  * main - main block
- * Return: 1
+ * Return: 0
  */
 int main(void)
 {
@@ -12,5 +12,5 @@ int main(void)
 	write(2,
 	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
 	      s);
-	return (1);
+	return (0);
 }
