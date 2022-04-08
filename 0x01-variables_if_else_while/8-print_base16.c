8c97c23734c64d2033113afcfc211e4283fc3578
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-08 15:08:00 -0700

    Add:8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 'a';
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar (a + '0');
+	}
+	for (b ='a'; b <= f; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
