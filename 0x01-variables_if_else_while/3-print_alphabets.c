c8272287a4c754a7c973e168e1ade3348faf635a
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-08 13:26:32 -0700

    Add:3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	char j = 'a';
+	char b = 'A';
+
+	while (j <= 'z')
+	{
+		putchar(j);
+		j++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++
+	}
+	putchar('\n');
+	return (0);
+}
