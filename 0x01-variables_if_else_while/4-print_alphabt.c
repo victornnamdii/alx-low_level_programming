efbaea0ca40224f44f2d55e4b6cf0d800c92a2a9
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-08 13:36:17 -0700

    Add:4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	char j = 'a';
+
+	while (j <= 'z')
+	{
+		if (j != 'q' && j!= 'e')
+		{
+			putchar(j);
+			j++;
+		}
+		putchar('\n');
+		return (0);
+	}
+}
