4ec04029cc68c6ebbee76d866de182e81801ca38
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-08 14:42:21 -0700

    Add:7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char k = 'z';
+
+	while (k >= a)
+	{
+		putchar(k);
+		k--;
+	}
+	putchar('\n');
+	return (0);
+}
