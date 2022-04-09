7b21c360b4adb226fe0da6e5c7cc302bb218340f
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-08 17:43:36 -0700

    Add:102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c, d, e, f;
+
+	a = 0;
+	d = 1;
+
+	while (a < 99)
+	{
+		b = a % 10;
+		c = a / 10;
+
+		putchar(c + '0');
+		putchar(b + '0');
+
+		while (d < 100)
+		{
+			e = d % 10;
+			f = d / 10;
+
+			if (e > b)
+			{
+				putchar(32);
+				putchar(f + '0');
+				putchar(e + '0');
+				
+				if (d < 100)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+			d++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
