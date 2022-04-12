8a1623b117b945e933a4cb39f134960aa805ae93
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 10:54:36 -0700

    Add:11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include  "main.h"
+/**
+ * print_to_98 - prints all numbers from n to 98
+ * @n: number it starts  from
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n < 99)
+		{
+			printf("%i", n);
+
+			if (n < 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%i", n);
+
+			if (n > 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+}
