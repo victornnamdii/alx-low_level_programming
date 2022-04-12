0c57cbb85d60effcadfa317d74070fbf82f01924
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 12:41:23 -0700

    Edit:100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,58 @@
 #include "main.h"
 /**
- * times_table - prints 9 times table
+ * print_times_table - prints n times table
+ * @n: the value of times table
  */
 void print_times_table(int n)
 {
 	int a = 0, j, k;
 
-	while (a < (n + 1))
+	if (n  > 15 || n < 0)
 	{
-		j = 0;
-
-		while (j < (n +1))
+		_putchar('\0');
+	}
+	else
+	{
+		while (a < (n + 1))
 		{
-			k = a * j;
+			j = 0;
 
-			if (k <= 9 && j != 0)
+			while (j < (n + 1))
 			{
-				_putchar(32);
-				_putchar(32);
-				_putchar(k + '0');
-			}
-			else if (j == 0)
-			{
-				_putchar(k + '0');
-			}
-			else
-			{
-				_putchar(32);
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
+				k = a * j;
 
+				if (k <= 9 && j != 0)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(k + '0');
+				}
+				else if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+				else if (k < 100)
+				{
+					_putchar(32);
+					_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
+
+				}
+				else
+				{
+					_putchar(k / 100);
+					_putchar((k / 10) % 10);
+					_putchar(k % 10);
+				}
+				if (j < n)
+				{
+					_putchar(44);
+					_putchar(32);
+				}
+				j++;
 			}
-			if (j < n)
-			{
-				_putchar(44);
-				_putchar(32);
-			}
-			j++;
+			_putchar('\n');
+			a++;
 		}
-		_putchar('\n');
-		a++;
 	}
 }
