ccf8df1fed604c1ecdadfe787f1842b8cd203527
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 06:20:22 -0700

    Add:8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d, e, f;
+
+	a = 0;
+
+	while (a < 24)
+	{
+		for (d = 0; d < 60; d++)
+		{
+			b = a % 10;
+			c = a / 10;
+			e = d % 10;
+			f = d / 10;
+
+			_putchar(c + '0');
+			_putchar(b + '0');
+			_putchar(58);
+			_putchar(f + '0');
+			_putchar(e + '0');
+			_putchar('\n');
+		}
+		a++;
+	}
+}
