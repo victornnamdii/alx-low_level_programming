e10120fc93e27cf8ae281e4cc650d583b1194367
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 12:20:33 -0700

    Add:12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times table
+ */
+void print_times_table(int n)
+{
+	int a = 0, j, k;
+
+	while (a < (n + 1))
+	{
+		j = 0;
+
+		while (j < (n +1))
+		{
+			k = a * j;
+
+			if (k <= n && j != 0)
+			{
+				_putchar(32);
+				_putchar(k + '0');
+			}
+			else if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+
+			}
+			if (j < n)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+			j++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
