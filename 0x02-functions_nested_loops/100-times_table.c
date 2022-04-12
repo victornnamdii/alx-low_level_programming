bd37987efd778a5c6a492a513e196e449f039a6f
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 13:04:28 -0700

    Edit:100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,7 @@ void print_times_table(int n)
 						_putchar(k / 10 + '0');
 						_putchar(k % 10 + '0');
 					}
-					else if
+					else
 					{
 						_putchar(k / 100 + '0');
 						_putchar((k / 10) % 10 + '0');
