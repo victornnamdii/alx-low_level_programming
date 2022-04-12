4e19f633a03406a4967eda1eb5f724447c15497e
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 14:40:13 -0700

    Add:102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * Return: 0
+ */
+int main (void)
+{
+	int a = 0, b = 1, n = 2, d;
+
+	printf("%i, %i", a, b);
+
+	while (n < 50)
+	{
+		a + b = d;
+		printf(", %i", d);
+		a = b;
+		b = d;
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
