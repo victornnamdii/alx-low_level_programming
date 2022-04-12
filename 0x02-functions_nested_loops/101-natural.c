efbc787a98c5d9c1a5a0b35f2b7a45b2a617468a
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 14:14:36 -0700

    Edit:101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,18 +5,13 @@
  */
 int main(void)
 {
-	int a = 0, c, k, m, n;
+	int a = 0, c = 0;
 
 	while (a < 1024)
 	{
-		c = 0;
-		m = a % 3;
-		n = a % 5;
-
-		if (m == 0 || n == 0)
+		if ((a % 5) == 0 || (a % 3) == 0)
 		{
-			scanf(a, &k);
-			c = c + k;
+			c = c + a;
 		}
 		a++;
 	}
