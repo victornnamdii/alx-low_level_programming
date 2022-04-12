d89a383553d5937c5a8ac3ba055d83f22efb4648
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 16:02:38 -0700

    Add:104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * Return: 0
+ */
+int main(void)
+{
+	long int a = 1, b = 2, n = 2, d;
+
+	printf("%lu, %lu", a, b);
+
+	while (n < 98)
+	{
+		d = a + b;
+		printf(", %lu", d);
+		a = b;
+		b = d;
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
