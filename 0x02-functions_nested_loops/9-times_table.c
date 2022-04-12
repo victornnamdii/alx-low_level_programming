077a65f902cb6dc03ad49b5add109ea17bbe6741
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 08:03:46 -0700

    Add:9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times table
+ */
+void times_table(void)
+{
+	int a = 0; j;
+	
+	while (a < 10)
+	{
+		j = 0;
+
+		while (j < 10)
+		{
+			_putchar((a + j) + 0);
+
+			if (j < 9)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+			}
+			j++;
+		}
+		a++;
+	}
+}
