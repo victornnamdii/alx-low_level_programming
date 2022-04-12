26b338fb32235bda9a619f47c1bcf02d95d71aec
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 03:33:13 -0700

    Add:0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints  putchar and new line
+ * Return: 0
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++
+	}
+	_putchar('\n');
+
+	return (0);
+}
