fe9a09e26f38f2fd4465d2928de7d71cbf28dcc9
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 04:08:17 -0700

    Add:2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - prints alphabets in 10 lines
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	int j;
+
+	while (j < 10)
+	{
+		j = 0;
+
+		while (a <= z)
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n')
+		j++;
+	}
+}
