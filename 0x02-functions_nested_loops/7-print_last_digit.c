e5894bfba1224e293ce775b3adef0320685179f5
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 05:55:23 -0700

    Edit:7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,5 +6,14 @@
  */
 int print_last_digit(int n)
 {
-	return (n % 10);
+	if (n < 0)
+	{
+		_putchar (((n * -1) % 10) + '0');
+		return ((n * -1) % 10);
+	}
+	else
+	{
+		_putchar ((n % 10) + '0');
+		return (n % 10);
+	}
 }
