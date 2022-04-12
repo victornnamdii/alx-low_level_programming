1b0efdc6fb9241ecd53e59e7119d93047d3f3a53
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 12:53:08 -0700

    Edit: 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,52 +7,47 @@ void print_times_table(int n)
 {
 	int a = 0, j, k;
 
-	if (n  > 15 || n < 0)
-	{
-		_putchar('\0');
-	}
-	else
+	if (n  < 15 && n > 0)
 	{
 		while (a < (n + 1))
-		{
-			j = 0;
-
-			while (j < (n + 1))
 			{
-				k = a * j;
+				j = 0;
 
-				if (k <= 9 && j != 0)
+				while (j < (n + 1))
 				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(k + '0');
-				}
-				else if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-				else if (k < 100)
-				{
-					_putchar(32);
-					_putchar(k / 10 + '0');
-					_putchar(k % 10 + '0');
+					k = a * j;
 
+					if (k <= 9 && j != 0)
+					{
+						_putchar(32);
+						_putchar(32);
+						_putchar(k + '0');
+					}
+					else if (j == 0)
+					{
+						_putchar(k + '0');
+					}
+					else if (k < 100)
+					{
+						_putchar(32);
+						_putchar(k / 10 + '0');
+						_putchar(k % 10 + '0');
+					}
+					else if (k > 99)
+					{
+						_putchar(k / 100);
+						_putchar((k / 10) % 10);
+						_putchar(k % 10);
+					}
+					if (j < n)
+					{
+						_putchar(44);
+						_putchar(32);
+					}
+					j++;
 				}
-				else
-				{
-					_putchar(k / 100);
-					_putchar((k / 10) % 10);
-					_putchar(k % 10);
-				}
-				if (j < n)
-				{
-					_putchar(44);
-					_putchar(32);
-				}
-				j++;
+				_putchar('\n');
+				a++;
 			}
-			_putchar('\n');
-			a++;
-		}
 	}
 }
