b37041b4245dde2c35177ccd58c6c1b6a248995c
victornnamdii <101352050+victornnamdii@users.noreply.github.com>
2022-04-13 01:40:34 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,13 +13,13 @@ int main(void)
 	printf("%lu, %lu, ", a, b);
 	for (count = 2; count < 98; count++)
 	{
-		if (a + b > LARGEST || y > 0 || x > 0)
+		if ((a + b) > LARGEST || y > 0 || x > 0)
 		{
 			hold1 = (a + b) / LARGEST;
 			hold2 = (a + b) % LARGEST;
 			hold3 = x + y + hold1;
 			x = y, y = hold3;
-			a = y, y = hold2;
+			a = b, b = hold2;
 			printf("%lu%010lu", y, b);
 		}
 		else
