44823a9eefbbb13ded85da57684f9466ec3bbe77
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 17:25:53 -0700

    edit:104

--- 0x01-variables_if_else_while/104-fibonacci.c
+++ 0x01-variables_if_else_while/104-fibonacci.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 /**
  * main - entry block
  * Return: 0
  */
 int main(void)
 {
-	unsigned long int a = 1, b = 2, n = 2, d, j = 10000000000;
+	unsigned long int a = 1, b = 2, n = 2, d;
 	unsigned long int hold1, hold2, x = 0, y = 0, hold3;
 
 	printf("%lu, %lu", a, b);
@@ -13,10 +14,10 @@ int main(void)
 	while (n < 50)
 	{
 		d = a + b;
-		if (d > j || x > 0 || y > 0)
+		if (d > LARGEST || x > 0 || y > 0)
 		{
-			hold1 = (a + b) / j;
-			hold2 = (a + b) % j;
+			hold1 = (a + b) / LARGEST;
+			hold2 = (a + b) % LARGEST;
 			hold3 = x + y + hold1;
 			x = y, y = hold3;
 			a = b, b = hold2;
