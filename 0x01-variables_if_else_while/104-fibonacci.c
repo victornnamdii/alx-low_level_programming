a265f9d3942ae74d5fbbb099e5e1a4d4415466bf
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-12 17:19:44 -0700

    Edit:104

---
+++ 0x01-variables_if_else_while/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * Return: 0
+ */
+int main(void)
+{
+	long int a = 1, b = 2, n = 2, d, j = 10000000000;
+	long int hold1, hold2, x = 0, y = 0, hold3;
+
+	printf("%lu, %lu", a, b);
+
+	while (n < 50)
+	{
+		d = a + b;
+		if (d > j)
+		{
+			hold1 = (a + b) / j;
+			hold2 = (a + b) % j;
+			hold3 = x + y + hold1;
+			x = y, y = hold3;
+			a = b, b = hold2;
+			printf(", %lu%010lu", y, b);
+		}
+		else
+		{
+			printf(", %lu", d);
+		}
+		a = b;
+		b = d;
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
