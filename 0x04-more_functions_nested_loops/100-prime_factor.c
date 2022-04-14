7b0e291a05c7956f8b7675c44b5286f45458dae8
victornnamdii <101352050+victornnamdii@users.noreply.github.com>
2022-04-14 20:59:15 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 /**
  * maxPrimeFactors - finds largest prime factor
  * @n: value to be checked
@@ -7,42 +7,38 @@
  */
 long long maxPrimeFactors(long long n)
 {
-	long long maxPrime = -1;
-	int  i;
-
-	while (n % 2 == 0)
-	{
-		maxPrime = 2;
-		n = n / 2;
-	}
-	while (n % 3 == 0)
-	{
-		maxPrime = 3;
-		n = n / 3;
-	}
-	for (i = 5; i < sqrt(n); i +=  6)
-	{
-		while (n % i == 0)
-		{
-			maxPrime = i;
-			n = n / 1;
-		}
-		while (n % (i + 2) == 0)
-		{
-			maxPrime = i + 2;
-			n = n / (i + 2);
-		}
-	}
-	if (n > 4)
-	{
-		maxPrime = n;
-	}
-	return (maxPrime);
+    long long maxPrime = -1;
+ 
+    while (n % 2 == 0)
+    {
+        maxPrime = 2;
+        n >>= 1; 
+    }
+    while (n % 3 == 0)
+    {
+        maxPrime = 3;
+        n = n / 3;
+    }
+    for (int i = 5; i <= sqrt(n); i += 6)
+    {
+        while (n % i == 0)
+        {
+            maxPrime = i;
+            n = n / i;
+        }
+        while (n % (i + 2) == 0)
+        {
+            maxPrime = i + 2;
+            n = n / (i + 2);
+        }
+    }
+    if (n > 4)
+        maxPrime = n;
+    return (maxPrime);
 }
 int main(void)
 {
-	long long n = 612852475143;
-
-	printf("%lld\n", maxPrimeFactors(n));
-	return (0);
+    long long n = 612852475143;
+    printf("%lld\n", maxPrimeFactors(n));
+    return (0);
 }
