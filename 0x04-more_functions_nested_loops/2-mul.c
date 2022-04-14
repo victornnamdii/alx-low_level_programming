3d8cd975829528127b099b2c4ca9460e2b63013c
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-14 05:25:42 -0700

    edot:2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,17 +3,9 @@
  * mul - multiplies 2 integers
  * @a: first integer
  * @b: second integer
- * Return: 0
+ * Return: result of multiplication
  */
 int mul(int a, int b)
 {
-	int i = 0;
-	char k[] = a * b;
-
-	while (k[i] != '\0')
-	{
-		_putchar(k[i] + '0');
-		i++;
-	}
-	return (0);
+	return (a * b);
 }
