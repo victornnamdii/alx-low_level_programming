8ba646efdbae19e6d03a76fd1c592ccd56a9ded6
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-14 06:01:50 -0700

    Add:5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - prints nmbrs from 0 to 14 in ten lines
+ */
+void more_numbers(void)
+{
+	int a = 0, j;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar(10);
+	}
+}
