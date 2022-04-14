921b3712f5d5c12e1903541e170ae4a5aaede819
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-14 04:50:41 -0700

    Edit:1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isdigit: checks if character is a digit
+ * _isdigit - checks if character is a digit
  * @c: character to be checked
  * Return: 1 if it is a digit, 0 otherwise
  */
