43fe56d61405ebb6a7d24ada5f2b55003cf02c15
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-14 05:43:46 -0700

    Add:4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers from 0 to 9 
+ * apart from 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
+	}
+	_putchar(10);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,4 @@ int _isdigit(int c);
 int _putchar(char c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
