75ed9a6d29e8e0985e8bc6d525c53c3fbf2cd681
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-14 08:20:25 -0700

    Add:9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i < 101)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			if (i < 100)
+			{
+				printf("%i ", i);
+			}
+			else
+			{
+				printf("%i", i);
+			}
+		}
+		i++;
+	}
+	printf("\n");
+}
