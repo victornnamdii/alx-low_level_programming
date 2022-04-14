3250d4dc836644a1af04d637558a239e30573d7d
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-14 05:03:03 -0700

    Edit:2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int i = 0;
+	char k;
+
+	k[] = a * b;
+
+	while (k[i] != '\0')
+	{
+		_putchar(k[i]);
+		i++;
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,4 @@
 int _isupper(int c);
 int _isdigit(int c);
+int _putchar(char c);
+int mul(int a, int b);
