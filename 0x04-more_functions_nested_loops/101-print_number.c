ece7970879c16988916c2ae20052f54ff25ebca4
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-14 14:23:59 -0700

    Add:101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_number - prints integer using putchar only
+ * @n: integer
+ */
+void print_number(int n)
+{
+	long m, num;
+	int c;
+	num = n;
+
+	if (num < 0)
+	{
+		_putchar(45);
+		num = -1 * num;
+	}
+	m = 1;
+	c = 1;
+
+	while (c)
+	{
+		if (num / (m * 10) > 0)
+			m = m * 10;
+		else
+			c = 0;
+	}
+	while (num >=0)
+	{
+		if (m == 1)
+		{
+			_putchar(num % 10 + '0');
+			num = -1;
+		}
+		else
+		{
+			_putchar(num / m % 10 + '0');
+			m = m / 10;
+		}
+	}
+}
