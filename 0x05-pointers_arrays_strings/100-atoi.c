7a52049cfff1311af950b9e5378d78f1d4845762
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-20 15:35:41 -0700

    Add:100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: char  type pointer
+ * Return: 0 if no number else the converted value
+ */
+int _atoi(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 48 && s[i] <= 57))
+		{
+			for (i = 0; s[i] != '\0'; i++)
+			{
+				if ((s[i] <= 57 && s[i] >= 48) || s[i] == 45)
+				{
+					_putchar(s[i]);
+				}
+			}
+		}
+		else
+			return (0);
+	}
+}
