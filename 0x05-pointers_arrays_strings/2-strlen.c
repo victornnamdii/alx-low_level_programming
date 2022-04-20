144edba8eb238b6c19b24d711b51bababe5e7d56
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-20 11:31:32 -0700

    Add:2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: char type string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
