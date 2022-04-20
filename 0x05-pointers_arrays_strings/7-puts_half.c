3f15e19f6f50a8e5785aec22ab8b9221c3503cd8
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-20 13:58:03 -0700

    Add:7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - prints 2nd half of a string
+ * @str: char type pointer
+ */
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	if (i % 2 != 0)
+		j = i--;
+	else
+		j = i - 2;
+	for (k = j / 2; str[k] != '\0';  k++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar(10);
+}
