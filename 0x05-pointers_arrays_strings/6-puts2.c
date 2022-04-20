3b857f6057b8c3fde64387ea05590ed8c9d49075
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-20 13:36:34 -0700

    Add:6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: char type pointer
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++);
+	i--;
+	j = i;
+	for (i = 0; i <= j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
