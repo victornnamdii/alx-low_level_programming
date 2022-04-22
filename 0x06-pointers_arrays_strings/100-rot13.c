4ff205886100271496580bbf09ade1cd440c30d7
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-22 12:26:46 -0700

    Add:100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @str: char type pointer
+ * Return: the updated string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+
+	char l[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char r[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (str[i] == l[j])
+				str[i] = r[j];
+		}
+	}
+	return (str);
+}
