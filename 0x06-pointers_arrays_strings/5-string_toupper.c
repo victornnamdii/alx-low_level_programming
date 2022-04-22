2c68a31c7fcfe743d56c998e4dc4198e6dffbeba
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-22 10:56:07 -0700

    Add:5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: char type  pointer
+ * Return: the updated string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for(i = 0; str[i] = '\0'; i++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
