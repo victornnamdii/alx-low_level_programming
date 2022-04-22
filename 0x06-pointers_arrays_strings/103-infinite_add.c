cdbd5e5581c35df4aa770905f08ca4f6e8f6ce0c
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-22 13:50:55 -0700

    Push:103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,17 +8,16 @@
 
 void rev_string(char *n)
 {
-  int i = 0;
-  int j = 0;
-  char temp;
+	int i = 0;
+	int j = 0;
+	char temp;
 
-  while (*(n + i) != '\0')
-  {
-    i++;
-  }
-  i--;
-
-  for (j = 0; j < i; j++, i--)
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (j = 0; j < i; j++, i--)
 	{
 		temp = *(n + j);
 		*(n + j) = *(n + i);
