e8c9df35cbe148ba26dcc5b7cac8e9534ea8a5ca
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-22 17:09:28 -0700

    Edit:1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 	k = i;
-	for (i = k, j = 0; j < n; i++, j++)
+	for (i = k, j = 0; j < n && src[j] != '\0'; i++, j++)
 	{
 		dest[i] = src[j];
 		dest[i + 1] = '\0';
