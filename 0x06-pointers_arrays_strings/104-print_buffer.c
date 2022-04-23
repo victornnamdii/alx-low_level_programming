7be1b422db50ac662f7bb49d97736c4dc23545a2
victornnamdii <101352050+victornnamdii@users.noreply.github.com>
2022-04-23 01:50:15 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,48 +1,49 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_buffer - prints a buffer
- * @b: char type pointer
- * @size: size of buffer
+ * print_buffer - prints buffer
+ * @b: char type buffer
+ * @size: size
  */
+
 void print_buffer(char *b, int size)
 {
-	int i, j, k, l;
-	int n = sizeof(b) / sizeof(char);
+	int o, j, i;
 
-	n--;
-	if (size  <= 0)
-		printf("\n");
-	else
+	o = 0;
+
+	if (size <= 0)
 	{
-		j = 0;
-		for (i = 0; i < n; i += 10)
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
 		{
-			j = i;
-			printf("%08x: ", i);
-			for (k = i; k >= i  && k < (i + 10); k +=  2)
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
 			{
-				if (k < n && (k + 1) < n)
-					printf("%x%x ", b[k], b[k +1]);
-				else if (k < n)
-					printf("%x", b[k]);
-				else
-				{
-					for (l = (8 - k) / 2; l > 0; l--)
-						printf("\t");
-				}
+				printf(" ");
 			}
-			while (i < (j + 10))
-			{
-				if (b[i] < 32)
-					printf(".");
-				else
-				{
-					printf("%c", b[i]);
-				}
-				i++;
-			}
-			printf("\n");
 		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
 	}
 }
