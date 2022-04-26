a0fc24de44ffdc7b78cd0d3d3618a1c128b42b7b
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-26 10:17:08 -0700

    Add:5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include  "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: char type pointer to string
+ * to be searched
+ * @needle: char type pointer to string
+ * to be searched for
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+	int m = 0;
+
+	for (i = 0; needle[i] != '\0'; i++)
+		;
+	k = i;
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] = needle[0])
+		{
+			m = 1;
+			for (j = 1; j < k; j++)
+			{
+				if (haystack[i + j] == needle[j])
+					m++;
+			}
+			if (m == k)
+				return (&s[i]);
+		}
+	}
+	return (0);
+}
