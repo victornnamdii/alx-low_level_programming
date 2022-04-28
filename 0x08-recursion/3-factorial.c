467b37347221720467001baec29a06d7228b6c6f
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-28 09:54:30 -0700

    Edit:3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,8 +8,8 @@ int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0 || n == 1)
+	else if (n == 0 || n == 1)
 		return (1);
-	if ((n - 1) > 0)
+	else if
 		return (n * factorial(n - 1));
 }
