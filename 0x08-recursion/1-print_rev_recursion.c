ea3563981fa126da342273b768ac166be79adc24
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-28 09:04:53 -0700

    Edit:1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,10 +5,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-	}
-	if (*(s + 1) != '\0')
 		_print_rev_recursion(s + 1);
-	_putchar(*s);
+		_putchar(*s);
+	}
 }
