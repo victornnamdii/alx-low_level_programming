765ca9cd4eef548686013b2c26a06030e546ee21
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-28 09:15:23 -0700

    Add:2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: char type pointerto the string
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s  != '\0')
+	{
+		i++;
+		_strlen_recursion(s + 1);
+	}
+	return (i);
+}
