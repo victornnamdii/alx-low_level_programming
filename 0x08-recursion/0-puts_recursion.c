6197c47154a41ca1e0e4b597a93cbd4d73d4fd2e
“victornnamdii” <victorilodiuba@gmail.com>
2022-04-28 08:18:58 -0700

    Add:0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: char type pointer to string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+	{
+		_putchar(10);
+	}
+}
