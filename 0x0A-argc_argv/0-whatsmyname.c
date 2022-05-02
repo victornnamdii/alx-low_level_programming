69701549897ea6503d317259b6528281446953c4
“victornnamdii” <victorilodiuba@gmail.com>
2022-05-02 11:59:39 -0700

    Add :0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
