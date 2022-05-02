b54e42ea9958aaae478f02c7418053b15be7e226
“victornnamdii” <victorilodiuba@gmail.com>
2022-05-02 12:25:01 -0700

    Add:3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - multiplies two numbers
+ * @argc: argument counter
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc,  char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%i\n", (atoi(argv[1]) * atoi(argv[2])));
+	return (0);
+}
