1402876308058b60037a9b16c35dd87252fc54d9
“victornnamdii” <victorilodiuba@gmail.com>
2022-05-02 12:08:23 -0700

    Add: 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: argument counter
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
