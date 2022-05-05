713446b51f1b4a98bb9a26514128e30c9b22ccd3
“victornnamdii” <victorilodiuba@gmail.com>
2022-05-05 15:37:35 -0700

    Edit:2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *nstr;
 	unsigned int i, j, size1, size2;
 
-	if (s1 == 0 &&  s2 != 0)
-		return (s2);
-	else if (s1 != 0 && s2 == 0)
-		return (s1);
-	else if (s1 == 0 && s2 == 0)
-		return (NULL);
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
 	for (size1 = 0; s1[size1] != '\0'; size1++)
 		;
 	for (size2 = 0; s2[size2] != '\0'; size2++)
