c34cf2c59035a2a653f009c7b7980bc4847b218b
“victornnamdii” <victorilodiuba@gmail.com>
2022-05-05 14:08:10 -0700

    Edit:1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 /**
  * _strdup - returns a pointer to a newly allocated space in memory,
  * which contains a copy of the string given as a parameter.
