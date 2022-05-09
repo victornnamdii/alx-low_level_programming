a62801f8c634cf481c75f668761f9861b35653b1
“victornnamdii” <victorilodiuba@gmail.com>
2022-05-09 11:25:30 -0700

    Edit:2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	k = malloc(nmemb * size);
 	if (k == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		*(k + 1) = 0;
 	return (k);
 }
