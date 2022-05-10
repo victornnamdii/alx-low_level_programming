804dc91e824c55bb5ad69beca1d7b0833d55996e
“victornnamdii” <victorilodiuba@gmail.com>
2022-05-10 16:08:05 -0700

    Add:101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - multiplies 2 numbers
+ * @argc: argument counter
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+}
