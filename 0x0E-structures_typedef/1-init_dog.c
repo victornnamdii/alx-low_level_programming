49cb53bfc04c226cfa47b48a4219b29a16c3be77
“victornnamdii” <victorilodiuba@gmail.com>
2022-05-11 15:39:08 -0700

    Add:1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d ==NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,5 +13,6 @@ struct dog
 	float age;
 	char *owner;
 } dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
