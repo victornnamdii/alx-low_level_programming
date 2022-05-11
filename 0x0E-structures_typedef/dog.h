fc04a73dc83f51ce68ea4d045de52016e110cbf4
“victornnamdii” <victorilodiuba@gmail.com>
2022-05-11 15:30:32 -0700

    Add:dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog data
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
 
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog_t;
 
 #endif
