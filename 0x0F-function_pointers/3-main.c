5a9aa91fad22fbfa2784ca9f6e848cc35f0fcb66
victornnamdii <101352050+victornnamdii@users.noreply.github.com>
2022-05-14 01:25:41 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,40 +1,44 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * main - performs simple operations
- * @argc: argument counter
- * @argv: argument array
- * Return: 0
- */
+ * main - Prints result of an operation
+ * @argc: Number of arguments
+ * @argv: Value of arguments
+ * Return: Returns an int.
+**/
+
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*m)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	m = get_op_func(argv[2]);
-	if (m == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	printf("%i\n", m(a, b));
-	return (0);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+if (
+(strcmp(argv[2], "+") &&
+strcmp(argv[2], "-") &&
+strcmp(argv[2], "*") &&
+strcmp(argv[2], "/") &&
+strcmp(argv[2], "%"))
+)
+{
+printf("Error\n");
+exit(99);
+}
+
+if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
+&& atoi(argv[3]) == 0)
+{
+printf("Error\n");
+exit(100);
+}
+
+printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+
+return (0);
 }
