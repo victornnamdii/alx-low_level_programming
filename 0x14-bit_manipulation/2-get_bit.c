08c35e4c05eb2381dbe0ad511e793d9349e3b5c0
“victornnamdii” <victorilodiuba@gmail.com>
2022-05-30 13:41:02 -0700

    Edit:2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	if (n & (1 << index) == 0)
+	if ((n & (1 << index)) == 0)
 		return (0);
 	return (1);
 }
