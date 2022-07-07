1b3d69301b927a55b851a9a3952ef1a2c7ef1d19
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-07 11:24:36 -0700

    Add: 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a list
+ * @h: doubly linked lists
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
