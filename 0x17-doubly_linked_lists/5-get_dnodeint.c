98b1fb317e8130b836f070b5a9a809215a671712
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-07 13:22:24 -0700

    Add: 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node of a doubly
+ * linked list
+ * @head: head of list
+ * @index: index of node
+ * Return: nth node or NULL if node doesn't exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *tmp = malloc(sizeof(dlistint_t));
+
+	if (tmp == NULL)
+		return(NULL);
+
+	for (i = 0; head != NULL; i++)
+	{
+		tmp = head;
+		head = head->next;
+		if (i == index)
+			return(tmp);
+	}
+
+	return(NULL);
+}
