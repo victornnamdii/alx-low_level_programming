3f2cb68a89536ffa8730a4eda927b2bf205148a2
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-12 13:24:58 -0700

    Edit(1): 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libtest.so
-LD_PRELOAD=/$PWD/libtest.so
+wget -P /tmp/ https://raw.githubusercontent.com/victornnamdii/alx-low_level_programming/main/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
