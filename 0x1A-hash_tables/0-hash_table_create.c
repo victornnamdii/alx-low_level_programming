fd8d2a4d938336ff808839bd6a51905c9de9bcef
“victornnamdii” <victorilodiuba@gmail.com>
2022-07-29 11:19:26 -0700

    Edit sumn

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,21 +6,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *new;
 
 	if (!size)
 		return (NULL);
-	new = calloc(1, sizeof(hash_table_t));
+	new = malloc(sizeof(hash_table_t));
 	if (!new)
 		return (NULL);
 	new->size = size;
-	new->array = calloc(size, sizeof(hash_node_t *));
+	new->array = malloc(size * sizeof(hash_node_t *));
 
 	if (!new->array)
 	{
 		free(new);
 		return (NULL);
 	}
+
+	for (i = 0; i < size; i++)
+		new->array[i] = NULL;
 
 	return (new);
 }
